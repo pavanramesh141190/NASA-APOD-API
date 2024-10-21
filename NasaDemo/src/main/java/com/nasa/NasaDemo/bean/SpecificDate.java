0cc0112b011526cdf97d8890150cf5aa8c18a35d
pavanramesh141190 <57172904+pavanramesh141190@users.noreply.github.com>
2024-10-21 12:58:51 +0530

    Refactored code 2

---
+++ NasaDemo/src/main/java/com/nasa/NasaDemo/bean/SpecificDate.java
@@ -0,0 +1,15 @@
+package com.nasa.NasaDemo.bean;
+
+public class SpecificDate {
+	private String date;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+}
---
+++ NasaDemo/src/test/java/com/nasa/NasaDemo/NasaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasa.NasaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
